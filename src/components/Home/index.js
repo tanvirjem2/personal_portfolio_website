966f88fb7f5b491e5a9caae50a657b72fc72f881
tanvirjem2 <tanviranjum046@gmail.com>
2024-03-28 14:14:30 +0600

    Home page done

---
+++ src/components/Home/index.js
@@ -0,0 +1,55 @@
+import './index.scss'
+import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
+
+const Home = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    const nameArray = ['h', 'o', 'z', 'i', 'b'];
+
+    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.'];
+
+    useEffect(() => {
+        (async () => {
+
+            return setTimeout(() => {
+                setLetterClass('text-animate-hover')
+            }, 4000)
+
+        })();
+
+    }, [])
+
+    return (
+        <div className='container home-page'>
+            <div className='text-zone'>
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <br />
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+                    <img src={LogoTitle} alt="developer" />
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={15}
+                    />
+                    <br />
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={22}
+                    />
+                </h1>
+                <h2>🚀 MERN Stack Developer / 💻 MERN / JS / HTML / CSS</h2>
+                <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+            </div>
+            <Logo />
+        </div>
+    )
+}
+
+export default Home;
